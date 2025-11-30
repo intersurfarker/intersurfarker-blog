507cfe7010fa36342aa89631245c51fc50ec41a7
intersurfarker <vvinformation@163.com>
2025-11-30 20:11:08 +0800

    base-change

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -14,7 +14,7 @@ import { defineUserConfig } from 'vuepress'
 import { plumeTheme } from 'vuepress-theme-plume'
 
 export default defineUserConfig({
-  base: '/blog/',
+  base: '/intersurfarker-blog/',
   lang: 'zh-CN',
   title: 'InterSurfArker',
   description: 'InterSurfArker',
